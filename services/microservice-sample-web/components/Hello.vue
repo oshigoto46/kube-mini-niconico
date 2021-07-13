8ab34cf713bd02e363e2a171e9db65a189f8d051
oshigoto46 <ppp.shiro46@gmail.com>
2021-07-13 08:02:31 +0000

    vue

--- services/microservice-sample-web/components/Hello.vue
+++ services/microservice-sample-web/components/Hello.vue
@@ -1,20 +1,34 @@
 <template>
-  <div id = "app">
-  "hoge"
-  {{message}}
-  </div>
+    <div>
+        <div>test: {{ test }}</div>
+        <div>test2: {{ test2 }}</div>
+        <div>test2: {{ test3 }}</div>
+        <div>test2: {{ test4 }}</div>
+        <div>test2: {{ hoges }}</div>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    data: {
-　   　message: 'Hello World'
-　  }
-  }
-}
+    async asyncData({app}) {
+      const res = await app.$axios.get('/hoge')
+        return {
+            hoges: "hoge" ,// res.data,
+            test: "asyncData",
+            test2: "asyncData2",
+            test3: "asyncData3"
+        };
+    },
+    data() {
+        return {
+            test: "data",
+            test2: "data2",
+            test4: "data4",
+            hoges : []
+        };
+    },
+};
 </script>
-
 <style>
 .name {
   font-size: 16px;
